cd2f15fe5a6882bb6910b5401ac86725d9328467
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-16 20:30:13 +0600

    Started using pandas library

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 #     print(data)
 
 #INSTEAD, WE CAN USE THIS:
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file) #comes out as a list that we can loop through **
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             new_temp = int(row[1])
+#             temperature.append(new_temp)
+#     print(temperature)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file) #comes out as a list that we can loop through **
-    temperature = []
-    for row in data:
-        if row[1] != "temp":
-            new_temp = int(row[1])
-            temperature.append(new_temp)
-    print(temperature)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data) #prints out the data in a tabular form perfectly
+
