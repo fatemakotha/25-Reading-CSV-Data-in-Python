3d0c31f842bde771327a9f40a4d2dde433d1cf0e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-16 20:51:40 +0600

    Dataframe to dictionary and series to list

--- main.py
+++ main.py
@@ -20,8 +20,15 @@
 
 import pandas
 #THERE ARE 2 TYPES OF OBJECT IN PANDAS: DATAFRAME and SERIES:
+#Each sheet of excel or google sheets is a DATAFRAME
+#Each list/column is a SERIES
 
 data = pandas.read_csv("weather_data.csv")
 print(type(data)) #we get a pandas dataframe object: <class 'pandas.core.frame.DataFrame'>
 print(data) #prints out the data in a tabular form perfectly
 print(type(data["temp"])) #we get a pandas series object: <class 'pandas.core.series.Series'>
+
+data_dict = data.to_dict()
+print(data_dict) #converts DATAFRAME into a dictionary
+
+temp_list = data["temp"].tolist() #converts a SERIESs into a list
