58e3cd344887cceee914723b3e231bfb5309fa79
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-16 20:17:58 +0600

    Imported csv library

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#THIS PROCESS WILL NEED STRIPPING AND CLEANING ETC:
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+#INSTEAD, WE CAN USE THIS:
+import csv
 
 with open("weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+    data = csv.reader(data_file) #comes out as a list that we can loop through **
+    for row in data:
+        print(row)
